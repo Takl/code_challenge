1d3265544eba90310b2c6ee105e0f482d21e9aef
Laura Alexander <lealexander@hearst.com>
2019-06-24 21:19:59 -0500

    Updated Docker build config

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,16 @@ COPY . /application
 WORKDIR /application
 
 COPY Gemfile Gemfile.lock ./
+
 ENV BUNDLER_VERSION 2.0.1
 
-RUN gem install bundler && bundle install --jobs 20 --retry 5
+RUN gem install bundler -v '2.0.1' 
+ENV BUNDLER_VERSION 2.0.1
+RUN bundle install --jobs 20 --retry 5
+
 
 COPY . .
 ENTRYPOINT ./entrypoint.sh
+EXPOSE 3000
+
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /application/tmp/pids/server.pid
+
 # Compile the assets
-bundle exec rake assets:precompile
+#bundle exec rake assets:precompile
 
 # Start the server
-bundle exec rails server
+#bundle exec rails server
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
+
